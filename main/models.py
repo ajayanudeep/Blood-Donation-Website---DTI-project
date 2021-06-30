10f8316ba9ae23627e6380ea27a8476d5cd9d897
ajay <ajay@pop-os.localdomain>
2021-06-30 21:50:18 +0530

    deploy

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class details(models.Model):
     name = models.CharField(max_length=50)
     gender = models.CharField(max_length=6, default="male")
     blood_type = models.CharField(max_length=3)
-    mobile = models.IntegerField()
+    mobile = models.BigIntegerField()
     hospital = models.CharField(max_length=50)
     address = models.CharField(max_length=200)
 
